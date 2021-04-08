de5e5e6010f40a12b432c6dfbc9974cca756fac8
jwakeGit <jwake@ucsc.edu>
2021-04-07 17:09:01 -0700

    Left & Right movement implemented!

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -31,12 +31,20 @@ class Play extends Phaser.Scene {
         borderUISize, game.config.height, 0xFFFFFF).setOrigin(0, 0);
 
         // add rocket (player 1)
-        this.plRocket = new Rocket(this, game.config.width/2, 
+        this.p1Rocket = new Rocket(this, game.config.width/2, 
         game.config.height - borderUISize - borderPadding, 'rocket').setOrigin(0.5, 0);
 
+        // define keys
+        keyF = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.F);
+        keyR = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.R);
+        keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
+        keyRIGHT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
     }
 
     update() {
         this.starfield.tilePositionX -= starSpeed;
+
+        // update rocket
+        this.p1Rocket.update();
     }
 }
